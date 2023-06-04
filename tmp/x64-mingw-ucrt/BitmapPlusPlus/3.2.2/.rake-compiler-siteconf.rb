a0d57ba410c7e097a971d9527471c2ca158a3ac1
Yordan Milkov Kostadinov <yordan_milkov@abv.bg>
2023-06-04 04:39:00 +0300

    remove temp build files

--- tmp/x64-mingw-ucrt/BitmapPlusPlus/3.2.2/.rake-compiler-siteconf.rb
+++
@@ -1,7 +0,0 @@
-require 'rbconfig'
-require 'mkmf'
-dest_path = mkintpath("D:/ChaosGroup/BitmapPlusPlusGem/lib/BitmapPlusPlus")
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
